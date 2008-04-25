a9d7befebee8a16df2145d61fd796a135f36c5cf
tommo4242 <tommo4242@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2008-04-25 22:06:52 +0000

    Export reports to excel format Issue : 22

--- trunk/admin_report_all.php
+++ trunk/admin_report_all.php
@@ -16,10 +16,10 @@ $contextUser = strtolower($_SESSION['contextUser']);
 //load local vars from superglobals
 $uid = isset($_REQUEST["uid"]) ? $_REQUEST["uid"]: 1;
 $orderby = isset($_REQUEST["orderby"]) ? $_REQUEST["orderby"]: "username";
-	
+
 //define the command menu
 include("timesheet_menu.inc");
-   
+
 // Calculate the previous month.
 $next_month = $month + 1;
 $next_year = $year;
@@ -49,30 +49,33 @@ if (!checkdate($prev_month, 1, $prev_year)) {
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
 		<td width="100%" class="face_padding_cell">
-		
+
 <!-- include the timesheet face up until the heading start section -->
 <? include("timesheet_face_part_1.inc"); ?>
 
 				<table width="100%" border="0">
 					<tr>
 						<td align="left" nowrap class="outer_table_heading">
-			  			<? echo date('F Y',mktime(0,0,0,$month,1,$year)) ?>
+						<? echo date('F Y',mktime(0,0,0,$month,1,$year)) ?>
+						</td>
+						<td  align="center" >
+						<a href="#" onclick="javascript:esporta('project')" ><img src="images/export_data.gif" name="esporta_dati" border=0></a>
 						</td>
 						<td align="right" nowrap>
 						<?
 							print "<a href=\"$_SERVER[PHP_SELF]?uid=$uid&month=$prev_month&year=$prev_year\" class=\"outer_table_action\">Prev</a>&nbsp;";
-			    		print "<a HREF=\"$_SERVER[PHP_SELF]?uid=$uid&month=$next_month&year=$next_year\" class=\"outer_table_action\">Next</a>";
+							print "<a HREF=\"$_SERVER[PHP_SELF]?uid=$uid&month=$next_month&year=$next_year\" class=\"outer_table_action\">Next</a>";
 						?>
 						</td>
 					</tr>
 				</table>
-				
+
 <!-- include the timesheet face up until the heading start section -->
 <? include("timesheet_face_part_2.inc"); ?>
 
 	<table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" class="outer_table">
 		<tr>
-			<td>			
+			<td>
 				<table width="100%" border="0" cellpadding="0" cellspacing="0" class="table_body">
 					<tr class="inner_table_head">
 						<td class="inner_table_column_heading">Name</td>
@@ -82,7 +85,7 @@ if (!checkdate($prev_month, 1, $prev_year)) {
 							<b><a href="<? echo $_SERVER["PHP_SELF"]; ?>?orderby=<? echo $PROJECT_TABLE; ?>.proj_id&month=<? echo $month; ?>&year=<? echo $year; ?>" class="inner_table_column_heading">Project</a>&nbsp;/&nbsp;
 							<a href="<? echo $_SERVER["PHP_SELF"]; ?>?orderby=<? echo $TASK_TABLE; ?>.task_id&month=<? echo $month; ?>&year=<? echo $year; ?>" class="inner_table_column_heading">Task</a></b>
 						</td>
-					</tr>				
+					</tr>
 <?
 
 	$query = "select distinct first_name, last_name, $USER_TABLE.username, $PROJECT_TABLE.title, $PROJECT_TABLE.proj_id, ".
@@ -95,6 +98,8 @@ if (!checkdate($prev_month, 1, $prev_year)) {
 	list ($qh,$num) = dbQuery($query);
 	$last_username = "";
 
+	$dati_total=array();
+	$dati_total_time_data=array();
 	if ($num == 0) {
 		print "	<tr>\n";
 		print "		<td align=\"center\">\n";
@@ -104,17 +109,19 @@ if (!checkdate($prev_month, 1, $prev_year)) {
 	}
 	else {
 		while ($name_data = dbResult($qh)) {
+			array_push($dati_total,$name_data);
 			$query = "SELECT sec_to_time(sum(unix_timestamp(end_time) - unix_timestamp(start_time))) AS diff " .
 				"FROM $TIMES_TABLE WHERE ".
-      "start_time >= '$year-$month-1' AND end_time < '$next_year-$next_month-1' and end_time > 0 ".
-      "and uid='$name_data[username]' AND task_id=$name_data[task_id] and proj_id=$name_data[proj_id]";
-      
+					"start_time >= '$year-$month-1' AND end_time < '$next_year-$next_month-1' and end_time > 0 ".
+					"and uid='$name_data[username]' AND task_id=$name_data[task_id] and proj_id=$name_data[proj_id]";
+
 			list($qh2, $num2) = dbQuery($query);
 			if ($num2 > 0)
 				$time_data = dbResult($qh2);
-    
+
+			array_push($dati_total_time_data,$time_data);
 			print "<tr>\n";
-    
+
 			if ($last_username != $name_data["username"]) {
 				$last_username = $name_data["username"];
 				print "<td class=\"calendar_cell_middle\">$name_data[first_name] $name_data[last_name]</TD>\n";
@@ -131,14 +138,24 @@ if (!checkdate($prev_month, 1, $prev_year)) {
 			else
 				print "&nbsp;";
 			print "</td>\n\n";
-    
-    $projectTitle = stripslashes($name_data["title"]);
-    $taskName = stripslashes($name_data["name"]);
+
+			$projectTitle = stripslashes($name_data["title"]);
+			$taskName = stripslashes($name_data["name"]);
 			print "<td class=\"calendar_cell_disabled_right\"><a href=\"javascript:void(0)\" ONCLICK=window.open(\"proj_info.php?proj_id=$name_data[proj_id]\",\"Info\",\"location=0,directories=no,status=no,menubar=no,resizable=1,scrollbar=yes,width=580,height=200\") class=\"outer_table_action\">$projectTitle</A> ".
-          "<a href=\"javascript:void(0)\" ONCLICK=window.open(\"task_info.php?proj_id=$name_data[proj_id]&task_id=$name_data[task_id]\",\"TaskInfo\",\"location=0,directories=no,status=no,scrollbar=yes,menubar=no,resizable=1,width=580,height=220\")>$taskName</A></TD>\n";
+				"<a href=\"javascript:void(0)\" ONCLICK=window.open(\"task_info.php?proj_id=$name_data[proj_id]&task_id=$name_data[task_id]\",\"TaskInfo\",\"location=0,directories=no,status=no,scrollbar=yes,menubar=no,resizable=1,width=580,height=220\")>$taskName</A></TD>\n";
 			print "</tr>\n";
 	}
 }
+	$out_js="<SCRIPT language=javascript> function esporta() {";
+	if (!empty($dati_total)) {
+		$_SESSION['excel_data']=$dati_total;
+		$_SESSION['excel_data_time_data']=$dati_total_time_data;
+		$out_js.="window.open('./export_timesheet.php?type=all')";
+	}else {
+		$out_js.="alert('No records to export')";
+	}
+	$out_js.="} </SCRIPT>";
+	echo 	$out_js;
 ?>
 				</TABLE>
 			</td>
@@ -151,7 +168,7 @@ if (!checkdate($prev_month, 1, $prev_year)) {
 		</td>
 	</tr>
 </table>
-		
+
 <?
 include ("footer.inc");
 ?>
--- trunk/admin_report_specific_user.php
+++ trunk/admin_report_specific_user.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // $Header: /cvsroot/tsheet/timesheet.php/admin_report_specific_user.php,v 1.11 2005/05/23 10:42:46 vexil Exp $
 
 // Authenticate
@@ -21,32 +21,32 @@ else
 
 //define the command menu
 include("timesheet_menu.inc");
-	
+
 // Set default months
 setReportDate($year, $month, $day, $next_week, $prev_week, $next_month, $prev_month, $time, $time_middle_month);
 
 function format_seconds($seconds) {
 	$temp = $seconds;
 	$hour = (int) ($temp / (60*60));
-	
+
 	if ($hour < 10)
 		$hour = '0'. $hour;
 
-	$temp -= (60*60)*$hour;    
+	$temp -= (60*60)*$hour;
 	$minutes = (int) ($temp / 60);
-  
+
 	if ($minutes < 10)
 		$minutes = '0'. $minutes;
 
-	$temp -= (60*$minutes);    
+	$temp -= (60*$minutes);
 	$sec = $temp;
-	
+
 	if ($sec < 10)
 		$sec = '0'. $sec;		// Totally wierd PHP behavior.  There needs to
-															// be a space after the . operator for this to work.
+								// be a space after the . operator for this to work.
 	return "$hour:$minutes:$sec";
 }
-  
+
 // Change the date-format for internationalization...
 if ($mode == "all") $mode = "monthly";
 if ($mode == "weekly") {
@@ -67,7 +67,7 @@ if ($mode == "weekly") {
 				"$PROJECT_TABLE.proj_id = $TIMES_TABLE.proj_id AND ".
 				"$TASK_TABLE.task_id = $TIMES_TABLE.task_id AND ".
 				"end_time < '".date('Y-m-d',$next_week)."' ".
-			"ORDER BY proj_id, task_id, start_time";							
+			"ORDER BY proj_id, task_id, start_time";
 } else {
 	$query = "SELECT $TIMES_TABLE.proj_id, ".
 				"$TIMES_TABLE.task_id, ".
@@ -86,12 +86,12 @@ if ($mode == "weekly") {
 				"$PROJECT_TABLE.proj_id = $TIMES_TABLE.proj_id AND ".
 				"$TASK_TABLE.task_id = $TIMES_TABLE.task_id AND ".
 				"end_time < '".date('Y-m-1',$next_month)."' ".
-			"ORDER BY proj_id, task_id, start_time";							
+			"ORDER BY proj_id, task_id, start_time";
 }
-//run the query  
+//run the query
 list($qh,$num) = dbQuery($query);
-  
-//define working variables  
+
+//define working variables
 $last_proj_id = -1;
 $last_task_id = -1;
 $total_time = 0;
@@ -115,7 +115,7 @@ $grand_total_time = 0;
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
 		<td width="100%" class="face_padding_cell">
-		
+
 <!-- include the timesheet face up until the heading start section -->
 <? include("timesheet_face_part_1.inc"); ?>
 
@@ -130,14 +130,17 @@ $grand_total_time = 0;
 									</td>
 								</tr>
 							</table>
-						</td>																
+						</td>
 						<td align="center" nowrap class="outer_table_heading">
 			  			<?	if ($mode == "weekly")
 								echo date('F d, Y',$time);
 							else
 								echo date('F Y',$time);
 						?>
-						</td>												
+						</td>
+						<td  align="center" >
+						<a href="#" onclick="javascript:esporta('user')" ><img src="images/export_data.gif" name="esporta_dati" border=0></a>
+						</td>
 						<td align="right" nowrap>
 						<?
 							printPrevNext($time, $next_week, $prev_week, $next_month, $prev_month, $time_middle_month,"uid=$uid", $mode);
@@ -151,9 +154,10 @@ $grand_total_time = 0;
 
 	<table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" class="outer_table">
 		<tr>
-			<td>			
+			<td>
 				<table width="100%" border="0" cellpadding="0" cellspacing="0" class="table_body">
 <?
+	$dati_total=array();
 	if ($num == 0) {
 		print "	<tr>\n";
 		print "		<td align=\"center\">\n";
@@ -163,6 +167,7 @@ $grand_total_time = 0;
 	}
 	else {
 		while ($data = dbResult($qh)) {
+			array_push($dati_total,$data);
 			// New project, so print out last project total time and start a new table cell.
 			if ($last_proj_id != $data["proj_id"]) {
 				$last_proj_id = $data["proj_id"];
@@ -171,8 +176,8 @@ $grand_total_time = 0;
 					print "<tr><td colspan=\"4\" align=\"right\" class=\"calendar_totals_line_weekly_right\">" .
 									"Total: <span class=\"calendar_total_value_weekly\">$formatted_time</span></td></tr>\n";
 				}
-    	  
-				$current_project_title = stripslashes($data["title"]);      
+
+				$current_project_title = stripslashes($data["title"]);
 				print "<tr><td valign=\"top\" colspan=\"4\" class=\"calendar_cell_disabled_right\">".
 							"<a href=\"javascript:void(0)\" onclick=\"javascript:window.open('proj_info.php?proj_id=$data[proj_id]','Project Info','location=0,directories=no,status=no,scrollbar=yes,menubar=no,resizable=1,width=500,height=200')\">$current_project_title</a>\n";
 							"</td></tr>\n";
@@ -188,22 +193,22 @@ $grand_total_time = 0;
 			print "&nbsp;</td>\n\t<td valign=\"top\" align=\"left\" width=\"8%\" class=\"calendar_cell_right\">$data[start_date]:&nbsp;&nbsp;</td>\n\t";
 			print "</td>\n\t<td valign=\"top\" align=\"left\" class=\"calendar_cell_right\">";
 			if ($data['log_message']) print stripslashes($data['log_message']);
-			else print "&nbsp;";	
+			else print "&nbsp;";
 			print "</td>\n\t";
 			print "<td valign=\"bottom\" align=\"right\" width=\"5%\" class=\"calendar_cell_right\">\n\t\t";
 			print "<a href=\"javascript:void(0)\" onclick=\"javascript:window.open(" .
-						"'trans_info.php?trans_num=$data[trans_num]'" . 
+						"'trans_info.php?trans_num=$data[trans_num]'" .
 						",'Task Event Info','location=0,directories=no" .
 						",status=no,scrollbar=yes,menubar=no,resizable=1,width=500,height=200')\">";
 			print "&nbsp;&nbsp;$data[diff_time]<a>\n\t</td>\n</tr>\n";
-			
+
 			//print "<a href=\"javascript:void(0)\" onclick=\"javascript:window.open('trans_info.php?trans_num=$data[trans_num]','Task Event Info','location=0,directories=no,status=no,scrollbar=yes,menubar=no,resizable=1,width=500,height=200')\">$data[start_date]: $data[diff_time]</a>\n";
 		//					"</tr></td>\n";
-			
+
 			$total_time += $data["diff"];
 			$grand_total_time += $data["diff"];
 		}
-  
+
 		if ($total_time) {
 			$formatted_time = format_seconds($total_time);
 			print "<tr><td colspan=\"4\" align=\"right\" class=\"calendar_totals_line_weekly_right\">" .
@@ -211,6 +216,16 @@ $grand_total_time = 0;
 		}
 		$formatted_time = format_seconds($grand_total_time);
 	}
+	$out_js="<SCRIPT language=javascript> function esporta() {";
+		if (!empty($dati_total)) {
+			$_SESSION['excel_data']=$dati_total;
+			$out_js.="window.open('./export_timesheet.php?type=user')";
+		}else {
+			$out_js.="alert('No records to export')";
+		}
+	$out_js.="} </SCRIPT>";
+
+	echo 	$out_js;
 
 ?>
 						</tr>
@@ -218,7 +233,7 @@ $grand_total_time = 0;
 				</table>
 			</td>
 		</tr>
-<? 
+<?
 	if ($num > 0) {
 ?>
 		<tr>
@@ -231,7 +246,7 @@ $grand_total_time = 0;
 		print "Weekly";
 	else
 		print "Monthly";
-?>						
+?>
 							total:
 							<span class="calendar_total_value_monthly"><? echo $formatted_time; ?></span>
 						</td>
@@ -243,14 +258,14 @@ $grand_total_time = 0;
 	}
 ?>
 	</table>
-	
+
 <!-- include the timesheet face up until the end -->
 <? include("timesheet_face_part_3.inc"); ?>
 
 		</td>
 	</tr>
 </table>
-		
+
 </form>
 <?
 include ("footer.inc");
