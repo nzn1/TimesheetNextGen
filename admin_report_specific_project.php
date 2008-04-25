3c16d6fb330eba0bde697650f75d2b7248d13e5f
tommo4242 <tommo4242@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2008-04-25 22:06:52 +0000

    Export reports to excel format Issue : 22

--- admin_report_specific_project.php
+++ admin_report_specific_project.php
@@ -27,38 +27,38 @@ if ($proj_id == 0)
 	//get the first project
 	$proj_id = getFirstProject();
 
-	
+
 // Set default months
 setReportDate($year, $month, $day, $next_week, $prev_week, $next_month, $prev_month, $time, $time_middle_month);
 
 function format_seconds($seconds) {
 	$temp = $seconds;
 	$hour = (int) ($temp / (60*60));
-	
+
 	if ($hour < 10)
 		$hour = '0'. $hour;
 
-	$temp -= (60*60)*$hour;    
+	$temp -= (60*60)*$hour;
 	$minutes = (int) ($temp / 60);
-  
+
 	if ($minutes < 10)
 		$minutes = '0'. $minutes;
 
-	$temp -= (60*$minutes);    
+	$temp -= (60*$minutes);
 	$sec = $temp;
-	
+
 	if ($sec < 10)
 		$sec = '0'. $sec;		// Totally wierd PHP behavior.  There needs to
 						// be a space after the . operator for this to work.
 	return "$hour:$minutes:$sec";
 }
-  
+
 //get the project name
-//	$query = "select title from $PROJECT_TABLE where proj_id='$proj_id'"; 
+//	$query = "select title from $PROJECT_TABLE where proj_id='$proj_id'";
 //	list($qh,$num) = dbQuery($query);
 //	$data = dbResult($qh);
-//	$project_title = $data['title'];	  
-  
+//	$project_title = $data['title'];
+
 // Change the date-format for internationalization...
 if ($mode == "all") $mode = "monthly";
 if ($mode == "weekly") {
@@ -83,10 +83,10 @@ if ($mode == "weekly") {
 			"end_time < '".date("Y-m-1",$next_month)."' order by $USER_TABLE.uid, task_id, start_time";
 }
 
-//run the query  
+//run the query
 list($qh,$num) = dbQuery($query);
-  
-//define working varibales  
+
+//define working variables
 $last_uid = -1;
 $last_task_id = -1;
 $total_time = 0;
@@ -111,7 +111,7 @@ $grand_total_time = 0;
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
 		<td width="100%" class="face_padding_cell">
-		
+
 <!-- include the timesheet face up until the heading start section -->
 <? include("timesheet_face_part_1.inc"); ?>
 
@@ -119,22 +119,25 @@ $grand_total_time = 0;
 					<tr>
 						<td align="left" nowrap width="250">
 							<table width="100%" height="100%" border="0" cellpadding="1" cellspacing="2">
-								<tr>											
+								<tr>
 									<td align="right" width="0" class="outer_table_heading">Project:</td>
 									<td align="left" width="100%">
 											<? project_select_droplist($proj_id); ?>
 									</td>
 								</tr>
 							</table>
-						</td>											
+						</td>
 						<td align="center" nowrap class="outer_table_heading">
-			  			<? 
+						<?
 							if ($mode == "weekly")
-								echo date('F d, Y',$time); 
+								echo date('F d, Y',$time);
 							else
 								echo date('F Y',$time);
 						?>
-						</td>												
+						</td>
+						<td  align="center" >
+						<a href="#" onclick="javascript:esporta('project')" ><img src="images/export_data.gif" name="esporta_dati" border=0></a>
+						</td>
 						<td align="right" nowrap>
 						<?
 							printPrevNext($time, $next_week, $prev_week, $next_month, $prev_month, $time_middle_month,"proj_id=$proj_id", $mode);
@@ -148,9 +151,10 @@ $grand_total_time = 0;
 
 	<table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" class="outer_table">
 		<tr>
-			<td>			
+			<td>
 				<table width="100%" border="0" cellpadding="0" cellspacing="0" class="table_body">
 <?
+	$dati_total=array();
 	if ($num == 0) {
 		print "	<tr>\n";
 		print "		<td align=\"center\">\n";
@@ -160,6 +164,7 @@ $grand_total_time = 0;
 	}
 	else {
 		while ($data = dbResult($qh)) {
+			array_push($dati_total,$data);
 			// New project, so print out last project total time and start a new table cell.
 			if ($last_uid != $data["uid"]) {
 				$last_uid = $data["uid"];
@@ -168,7 +173,7 @@ $grand_total_time = 0;
 					print "<tr><td colspan=\"4\" align=\"right\" class=\"calendar_totals_line_weekly_right\">" .
 									"Total: <span class=\"calendar_total_value_weekly\">$formatted_time</span></td></tr>\n";
 				}
-      
+
 				print "<tr>\n\t<td colspan=\"4\" valign=\"top\" class=\"calendar_cell_disabled_right\">" .
 							stripslashes("$data[first_name] $data[last_name]")."</td></tr>\n";
 				$total_time = 0;
@@ -182,18 +187,18 @@ $grand_total_time = 0;
 			print "&nbsp;</td>\n\t<td valign=\"top\" align=\"left\" width=\"8%\" class=\"calendar_cell_right\">$data[start_date]:&nbsp;&nbsp;</td>\n\t";
 			print "</td>\n\t<td valign=\"top\" align=\"left\" class=\"calendar_cell_right\">";
 			if ($data['log_message']) print stripslashes($data['log_message']);
-			else print "&nbsp;";	
+			else print "&nbsp;";
 			print "</td>\n\t";
 			print "<td valign=\"bottom\" align=\"right\" width=\"5%\" class=\"calendar_cell_right\">\n\t\t";
 			print "<a href=\"javascript:void(0)\" onclick=\"javascript:window.open(" .
-						"'trans_info.php?trans_num=$data[trans_num]'" . 
+						"'trans_info.php?trans_num=$data[trans_num]'" .
 						",'Task Event Info','location=0,directories=no" .
 						",status=no,scrollbar=yes,menubar=no,resizable=1,width=500,height=200')\">";
 			print "&nbsp;&nbsp;$data[diff_time]<a>\n\t</td>\n</tr>\n";
 			$total_time += $data["diff"];
 			$grand_total_time += $data["diff"];
 		}
-  
+
 		if ($total_time) {
 			$formatted_time = format_seconds($total_time);
 			print "<tr><td colspan=\"4\" align=\"right\" class=\"calendar_totals_line_weekly_right\">" .
@@ -202,13 +207,24 @@ $grand_total_time = 0;
 		$formatted_time = format_seconds($grand_total_time);
 	}
 
+	$out_js="<SCRIPT language=javascript> function esporta() {";
+		if (!empty($dati_total)) {
+			$_SESSION['excel_data']=$dati_total;
+			$out_js.="window.open('./export_timesheet.php?type=project')";
+		}else {
+			$out_js.="alert('No records to export')";
+		}
+	$out_js.="} </SCRIPT>";
+
+	echo 	$out_js;
+
 ?>
 						</tr>
 					</td>
 				</table>
 			</td>
 		</tr>
-<? 
+<?
 	if ($num > 0)
 	{
 ?>
@@ -222,7 +238,7 @@ $grand_total_time = 0;
 		print "Weekly";
 	else
 		print "Monthly";
-?>						
+?>
 							total:
 							<span class="calendar_total_value_monthly"><? echo $formatted_time; ?></span>
 						</td>
@@ -234,14 +250,14 @@ $grand_total_time = 0;
 	}
 ?>
 	</table>
-	
+
 <!-- include the timesheet face up until the end -->
 <? include("timesheet_face_part_3.inc"); ?>
 
 		</td>
 	</tr>
 </table>
-		
+
 </form>
 <?
 include ("footer.inc");
