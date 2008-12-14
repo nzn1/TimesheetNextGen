025da5228d18728c6ea2299fb0616feb3b411695
ibrow <ibrow@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2008-12-14 00:48:36 +0000

    Experimental installer, based on r33 of the trunk
    This is a very basic web based installer and should be considered 
    experimental.
    
    It tried to detect if Timesheet NG is installed, if not, takes user to 
    the installer. If Timesheet NG is installed, but of a lower version, it 
    takes user to an (as yet non-working) upgrade page.
    
    The timesheet.sql.in file is now deprecated, the installation SQL being 
    moved to install/index.php, as is the install.sh file.
    
    To install a clean copy of Timesheet NG, simply checkout this version 
    from SVN into a directory within your web server, navigate in your 
    browser to that directory and you should be automatically redirected to 
    the web based installation. This page will have instructions to proceed.
    
    Todo:
    1. Get the "Check Database Connection" ajax functionality working
    2. Allow for the creation of the database
    3. Allow for the upgrading from previous versions
    4. Tidy up now unused *.inc.in files

---
+++ branches/exp_installer/dev/changeTimesLogMessageToText.sql
@@ -0,0 +1,2 @@
+ ALTER TABLE `timesheet_times` 
+ CHANGE `log_message` `log_message` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL  
---
+++ branches/exp_installer/report_all.php
@@ -0,0 +1,184 @@
+<?php
+// $Header: /cvsroot/tsheet/timesheet.php/report_all.php,v 1.5 2005/05/23 10:42:46 vexil Exp $
+
+// Authenticate
+require("class.AuthenticationManager.php");
+require("class.CommandMenu.php");
+if (!$authenticationManager->isLoggedIn() || !$authenticationManager->hasAccess('aclReports')) {
+	Header("Location: login.php?redirect=$_SERVER[PHP_SELF]&clearanceRequired=" . get_acl_level('aclReports'));
+	exit;
+}
+
+// Connect to database.
+$dbh = dbConnect();
+$contextUser = strtolower($_SESSION['contextUser']);
+
+//load local vars from superglobals
+$uid = isset($_REQUEST["uid"]) ? $_REQUEST["uid"]: 1;
+$orderby = isset($_REQUEST["orderby"]) ? $_REQUEST["orderby"]: "username";
+
+//define the command menu
+include("timesheet_menu.inc");
+
+// Calculate the previous month.
+$next_month = $month + 1;
+$next_year = $year;
+$prev_month = $month - 1;
+$prev_year = $year;
+
+//rollover year forward
+if (!checkdate($next_month, 1, $next_year)) {
+	$next_month -= 12;
+	$next_year ++;
+}
+
+//rollover year back
+if (!checkdate($prev_month, 1, $prev_year)) {
+	$prev_month += 12;
+	$prev_year --;
+}
+
+?>
+<html>
+<head><title>Report: All hours this month</title>
+<?php include ("header.inc"); ?>
+</head>
+<body <? include ("body.inc"); ?> >
+<?php include ("banner.inc"); ?>
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td width="100%" class="face_padding_cell">
+
+<!-- include the timesheet face up until the heading start section -->
+<? include("timesheet_face_part_1.inc"); ?>
+
+				<table width="100%" border="0">
+					<tr>
+						<td align="left" nowrap class="outer_table_heading">
+						<? echo date('F Y',mktime(0,0,0,$month,1,$year)) ?>
+						</td>
+						<td  align="center" >
+						<a href="#" onclick="javascript:esporta('project')" ><img src="images/export_data.gif" name="esporta_dati" border=0></a>
+						</td>
+						<td align="right" nowrap>
+						<?
+							print "<a href=\"$_SERVER[PHP_SELF]?uid=$uid&month=$prev_month&year=$prev_year\" class=\"outer_table_action\">Prev</a>&nbsp;";
+							print "<a HREF=\"$_SERVER[PHP_SELF]?uid=$uid&month=$next_month&year=$next_year\" class=\"outer_table_action\">Next</a>";
+						?>
+						</td>
+					</tr>
+				</table>
+
+<!-- include the timesheet face up until the heading start section -->
+<? include("timesheet_face_part_2.inc"); ?>
+
+	<table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" class="outer_table">
+		<tr>
+			<td>
+				<table width="100%" border="0" cellpadding="0" cellspacing="0" class="table_body">
+					<tr class="inner_table_head">
+						<td class="inner_table_column_heading">Name</td>
+						<td class="inner_table_column_heading"><a href="<? echo $_SERVER["PHP_SELF"]; ?>?orderby=username&month=<? echo $month; ?>&year=<? echo $year; ?>" class="inner_table_column_heading">Username</a></td>
+						<td class="inner_table_column_heading">Hours</td>
+						<td class="inner_table_column_heading">
+							<b><a href="<? echo $_SERVER["PHP_SELF"]; ?>?orderby=proj_id&month=<? echo $month; ?>&year=<? echo $year; ?>" class="inner_table_column_heading">Project</a>&nbsp;/&nbsp;
+							<a href="<? echo $_SERVER["PHP_SELF"]; ?>?orderby=task_id&month=<? echo $month; ?>&year=<? echo $year; ?>" class="inner_table_column_heading">Task</a></b>
+						</td>
+					</tr>
+<?
+
+	$query = "SELECT DISTINCT first_name, last_name, $USER_TABLE.username, $PROJECT_TABLE.title, ".
+			"$PROJECT_TABLE.proj_id, $TASK_TABLE.name, $TASK_TABLE.task_id ".
+		"FROM $USER_TABLE, $PROJECT_TABLE, $TASK_TABLE, $ASSIGNMENTS_TABLE, $TASK_ASSIGNMENTS_TABLE ".
+		"WHERE $ASSIGNMENTS_TABLE.proj_id = $PROJECT_TABLE.proj_id ".
+		"AND $TASK_ASSIGNMENTS_TABLE.task_id = $TASK_TABLE.task_id ".
+		"AND $PROJECT_TABLE.proj_id = $TASK_TABLE.proj_id ".
+		"AND $ASSIGNMENTS_TABLE.username = $USER_TABLE.username ".
+		"AND $USER_TABLE.username NOT IN ('admin','guest') ";
+	if ($orderby=="username")
+		$query .= "ORDER BY username,$PROJECT_TABLE.proj_id,$TASK_TABLE.task_id";
+	else if ($orderby=="proj_id")
+		$query .= "ORDER BY $PROJECT_TABLE.proj_id,$TASK_TABLE.task_id,username";
+	else //($orderby=="task_id")
+		$query .= "ORDER BY $TASK_TABLE.task_id,$PROJECT_TABLE.proj_id,username";
+
+	list ($qh,$num) = dbQuery($query);
+	$last_username = "";
+
+	$dati_total=array();
+	$dati_total_time_data=array();
+	if ($num == 0) {
+		print "	<tr>\n";
+		print "		<td align=\"center\">\n";
+		print "			<i><br>No hours recorded.<br><br></i>\n";
+		print "		</td>\n";
+		print "	</tr>\n";
+	}
+	else {
+		while ($name_data = dbResult($qh)) {
+			array_push($dati_total,$name_data);
+			$query = "SELECT sec_to_time(sum(unix_timestamp(end_time) - unix_timestamp(start_time))) AS diff " .
+				"FROM $TIMES_TABLE WHERE ".
+					"start_time >= '$year-$month-1' AND end_time < '$next_year-$next_month-1' and end_time > 0 ".
+					"and uid='$name_data[username]' AND task_id=$name_data[task_id] and proj_id=$name_data[proj_id]";
+
+			list($qh2, $num2) = dbQuery($query);
+			if ($num2 > 0)
+				$time_data = dbResult($qh2);
+
+			array_push($dati_total_time_data,$time_data);
+			print "<tr>\n";
+
+			if ($last_username != $name_data["username"]) {
+				$last_username = $name_data["username"];
+				print "<td class=\"calendar_cell_middle\">$name_data[first_name] $name_data[last_name]</TD>\n";
+				print "<td class=\"calendar_cell_middle\"><A HREF=\"report_specific_user.php?uid=$name_data[username]&month=$month&year=$year\">$name_data[username]</A></TD>\n";
+			}
+			else {
+				print "<td class=\"calendar_cell_middle\">&nbsp;</td>\n";
+				print "<td class=\"calendar_cell_middle\">&nbsp;</td>\n";
+				}
+
+			print "<td class=\"calendar_cell_middle\" align=\"center\">";
+			if ($num2 > 0 && isset($time_data["diff"]))
+				echo $time_data["diff"];
+			else
+				print "&nbsp;";
+			print "</td>\n\n";
+
+			$projectTitle = stripslashes($name_data["title"]);
+			$taskName = stripslashes($name_data["name"]);
+			print "<td class=\"calendar_cell_disabled_right\"><a href=\"javascript:void(0)\" ONCLICK=window.open(\"proj_info.php?proj_id=$name_data[proj_id]\",\"Info\",\"location=0,directories=no,status=no,menubar=no,resizable=1,scrollbar=yes,width=580,height=200\") class=\"outer_table_action\">$projectTitle</A> ".
+				"<a href=\"javascript:void(0)\" ONCLICK=window.open(\"task_info.php?proj_id=$name_data[proj_id]&task_id=$name_data[task_id]\",\"TaskInfo\",\"location=0,directories=no,status=no,scrollbar=yes,menubar=no,resizable=1,width=580,height=220\")>$taskName</A></TD>\n";
+			print "</tr>\n";
+	}
+}
+	$out_js="<SCRIPT language=javascript> function esporta() {";
+	if (!empty($dati_total)) {
+		$_SESSION['excel_data']=$dati_total;
+		$_SESSION['excel_data_time_data']=$dati_total_time_data;
+		$out_js.="window.open('./export_timesheet.php?type=all')";
+	}else {
+		$out_js.="alert('No records to export')";
+	}
+	$out_js.="} </SCRIPT>";
+	echo $out_js;
+?>
+				</TABLE>
+			</td>
+		</tr>
+	</table>
+
+<!-- include the timesheet face up until the end -->
+<? include("timesheet_face_part_3.inc"); ?>
+
+		</td>
+	</tr>
+</table>
+
+<?
+include ("footer.inc");
+?>
+</BODY>
+</HTML>
