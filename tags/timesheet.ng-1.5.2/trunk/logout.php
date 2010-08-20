9317f516b7e1a55d7ac7de5689b39e9a85eabc70
slmi113r <slmi113r@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2010-08-20 16:30:13 +0000

    removing bad 1.5.2

--- tags/timesheet.ng-1.5.2/trunk/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require("class.AuthenticationManager.php");
-//log the user out
-$authenticationManager->logout();
-//go to the login page
-Header("Location: login.php");
-
-?>
--- trunk/dev/addLDAPBindByUserToConfigTable.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `timesheet_config` 
-ADD `LDAPBindByUser` tinyint(4) NOT NULL DEFAULT '0' AFTER `LDAPBindPassword` ;
