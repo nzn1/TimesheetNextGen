66667f1480f36e0d29423f1aaeeeaa9773f81183
wrightsonm <wrightsonm@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2010-11-27 01:53:27 +0000

    globals.class now sets month and year from the get variable 'date1'
    monthly.php now changes its date using the css datetimepicker

--- branches/txsheet-2.0-demo/globals.class.php
+++ branches/txsheet-2.0-demo/globals.class.php
@@ -23,6 +23,59 @@ class gbl{
 		self::$year = isset($_REQUEST["year"]) ? $_REQUEST["year"]: self::$realToday["year"];
 		self::$month = isset($_REQUEST["month"]) ? $_REQUEST["month"]: self::$realToday["mon"];
 		self::$day = isset($_REQUEST["day"]) ? $_REQUEST["day"]: self::$realToday["mday"];
+
+	if(isset($_GET['date1'])){
+//if the date variable exists
+
+		/* @TODO - add validation to this!!!*/
+$d = explode('-',$_GET['date1']);
+
+	gbl::setYear($d[2]);
+	switch($d[1]){
+		case 'Jan':
+			self::$month = 1;
+			break;
+		case 'Feb':
+			self::$month = 2;
+			break;
+		case 'Mar':
+			self::$month = 3;
+			break;
+		case 'Apr':
+			self::$month = 4;
+			break;
+		case 'May':
+			self::$month = 5;
+			break;
+		case 'Jun':
+			self::$month = 6;
+			break;
+		case 'Jul':
+			self::$month = 7;
+			break;
+		case 'Aug':
+			self::$month = 8;
+			break;
+		case 'Sep':
+			self::$month = 9;
+			break;
+		case 'Oct':
+			self::$month = 10;
+			break;
+		case 'Nov':
+			self::$month = 11;
+			break;
+		case 'Dec':
+			self::$month = 12;
+			break;
+		default:
+			break;
+			
+	}
+
+	
+}
+		
 		self::$mode = isset($_REQUEST["mode"]) ? $_REQUEST["mode"]: 0;
 		self::$proj_id = isset($_REQUEST["proj_id"]) ? $_REQUEST["proj_id"]: 0;
 		self::$task_id = isset($_REQUEST["task_id"]) ? $_REQUEST["task_id"]: 0;
@@ -37,9 +90,15 @@ class gbl{
 	}
 	public static function getYear(){
 		return self::$year;
+	}
+	public static function setYear($i){
+		self::$year = $i;
 	}	
 	public static function getMonth(){
 		return self::$month;
+	}
+	public static function setMonth($i){
+		self::$month = $i;
 	}
 	public static function getDay(){
 		return self::$day;
