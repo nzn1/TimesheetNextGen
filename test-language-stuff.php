33e4c769bc9d7292889b1a945cf3481e0cfd75c0
slmi113r <slmi113r@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2011-03-08 22:18:46 +0000

    eliminated need for JObject class, cleaned up unneeded stuff from JLanguage class

--- test-language-stuff.php
+++ test-language-stuff.php
@@ -11,11 +11,14 @@ if (!class_exists('JLoader')) {
 //
 
 // Base classes.
-jimport('jclasses.object');
+//jimport('jclasses.object');
 
 
 // Factory class and methods.
 jimport('jclasses.factory');
+
+//JText class
+jimport('jclasses.text');
 
 // Register class that don't follow one file per class naming conventions.
 //JLoader::register('JObject', dirname(__FILE__).DS.'test-classes'.DS.'object.php');
@@ -23,7 +26,8 @@ jimport('jclasses.factory');
 //JLoader::register('JLanguage', dirname(__FILE__).DS.'test-classes'.DS.'language.php');
 //JLoader::register('JXMLElement', dirname(__FILE__).DS.'test-classes'.DS.'xmlelement.php');
 
-JLoader::register('JText', dirname(__FILE__).DS.'include'.DS.'jclasses'.DS.'methods.php');
+//JLoader::register('JText', dirname(__FILE__).DS.'include'.DS.'jclasses'.DS.'methods.php');
+
 JLoader::register('JPath', dirname(__FILE__).DS.'include'.DS.'jclasses'.DS.'filesystem.php');
 JLoader::register('JFolder', dirname(__FILE__).DS.'include'.DS.'jclasses'.DS.'filesystem.php');
 JLoader::register('JFile', dirname(__FILE__).DS.'include'.DS.'jclasses'.DS.'filesystem.php');
@@ -34,6 +38,7 @@ ppr(JLoader::getClasses());
 $lang = JFactory::getLanguage();
 ppr($lang);
 
-echo JText::_('JLIB_ERROR_INFINITE_LOOP')."\n";
+echo JText::_('JLIB_ERROR_INFINITE_LOOP')."\n<br>";
 echo JText::sprintf('JERROR_TABLE_BIND_FAILED',"These are a couple of JText tests")."\n";
+
 ?>
