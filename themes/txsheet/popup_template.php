1e8b24d0c2867d5a1037feabd41c41af211424fa
wrightsonm <wrightsonm@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2011-04-12 11:13:47 +0000

    added popup template

---
+++ themes/txsheet/popup_template.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+  <meta http-equiv="Content-Style-Type" content="text/css" />
+  {head}
+  <link rel="stylesheet" href="<?php echo PageElements::getRelThemePath();?>/styles/popup.css" type="text/css" />
+<!--This is an IE hack that allows absolute positioning inside a relative div-->
+<!--[if IE]>
+  <style type="text/css">
+    .layout {
+    	height: 0;
+    	he\ight: auto;
+    	zoom: 1;
+    }
+  </style>
+<![endif]-->
+
+<style type="text/css">
+  html{ overflow-y:Scroll;}
+</style>
+</head>
+
+<body {onload}>
+  {debugInfoTop}
+  {templateParserDebug}
+  <div align="center">
+    <div id="wrap">       
+    <div class="popup_content">
+      
+      {response}
+      {content}
+    </div><!--end content-->  
+    
+
+      
+    </div>
+    <!--end wrap-->
+  </div>
+  <!--end center-->
+<?
+//tags in index.php instead
+/*
+</body>
+</html>
+*/
+?>
