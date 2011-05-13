7b85870f3cf8dc87dae820d84cab8666a6a4a31c
peterlazarus <peterlazarus@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2011-05-13 03:19:01 +0000

    Added print_r function to print arrays

--- include/tsx/debuglog.php
+++ include/tsx/debuglog.php
@@ -9,6 +9,17 @@ class LogFile{
 			return( false );
 		}
 	}
+	public static function print_r($the_array ) {
+		if( $fh = @fopen("/tmp/php-debug.log", "a+" ) ) {
+			foreach ($the_array as $key => $value) {
+    			fputs( $fh, "Key: $key; Value: $value\n" );
+			}
+			fclose( $fh );
+			return( true );
+		} else {
+			return( false );
+		}
+	}
 }
 
 // vim:ai:ts=4:sw=4
