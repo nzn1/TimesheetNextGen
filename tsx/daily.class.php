57bc737302eeeb01d8e5a762a3459d01e476f355
peterlazarus <peterlazarus@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2011-08-10 03:11:11 +0000

    Additional modifications including changing the way the date selector works

--- tsx/daily.class.php
+++ tsx/daily.class.php
@@ -10,7 +10,11 @@ public function make_daily_link($ymdStr, $projId, $string) {
 }
 
 public function open_cell_middle_td() {
+<<<<<<< .mine
+	echo "<td class=\"alignmiddle\">";
+=======
 	echo "<td class=\"calendar_cell_middle\" align=\"right\">";
+>>>>>>> .r338
 }
 	
 }
