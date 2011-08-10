c26f90e63fa5a6da765475d433a8f9321634cc2c
peterlazarus <peterlazarus@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2011-08-10 03:40:26 +0000

    Bug resolution

--- branches/txsheet-2.0-demo/tsx/expenses/exp_add.php
+++ branches/txsheet-2.0-demo/tsx/expenses/exp_add.php
@@ -24,6 +24,10 @@ $start_day = date("d", $startDate);
 $start_month = date("n", $startDate);
 $start_year = date("Y", $startDate);;
 
+// get a list of expense categories
+	$query = "SELECT cat_id, description FROM ". tbl::getExpenseCategoryTable();
+	list($qcat, $num_cat) = dbQuery($query);
+
 ?>
 
 <form action="<?php echo Config::getRelativeRoot(); ?>/expenses/exp_add_action" method="post">
@@ -37,15 +41,27 @@ $start_year = date("Y", $startDate);;
 		<td>	<h1><?php echo JText::_('ADD_EXPENSE'); ?></h1>
 		</td>
 	</tr>
-	<!--  table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" class="outer_table" -->
 	<tr>
-				<!--  table width="100%" border="0" cellpadding="1" cellspacing="2" class="table_body" -->
-		<td align="right"><?php echo JText::_('PROJECT'); ?>:</td>
-		<td><input type="text" name="title" size="42" maxlength="200" /></td>
-	</tr>
 	<tr>
 		<td align="right"><?php echo JText::_('CLIENT'); ?>:</td>
-		<td><?php Common::client_select_list($client_id, 0, false, false, false, true, "", false); ?></td>
+		<td><?php Common::client_select_list($client_id, 0, false, false, false, true, "", "submit();"); ?></td>
+	</tr>
+		<td align="right"><?php echo JText::_('PROJECT'); ?>:</td>
+		<td><?php Common::project_select_list(gbl::getClientId(), false, gbl::getProjId(), gbl::getContextUser(), false, true, false); ?></td>
+		<td>&nbsp;</td>
+	</tr>
+	</tr>
+		<td align="right"><?php echo JText::_('CATEGORY'); ?>:</td>
+		<td>
+			<select name="category">
+			<?php 
+				for ($i=1; $i<=$num_cat; $i++) {
+					$data = dbResult($qcat);
+					print "<option value=\"". $data['cat_id'] . "\">" . $data['description'] . "</option>";
+				}
+			?>
+
+			</select>
 	</tr>
 	<tr>
 		<td align="right" valign="top"><?php echo JText::_('DESCRIPTION'); ?>:</td>
@@ -53,19 +69,25 @@ $start_year = date("Y", $startDate);;
 	</tr>
 	<tr>
 		<td align="right"><?php echo JText::_('DATE_EXPENSE_INCURRED'); ?>:</td>
-		<td><?php Common::day_button("start_day",0,0); Common::month_button("start_month", $start_month); Common::year_button("start_year", $start_year); ?></td>
+		<td><?php Common::day_button("exp_day", $start_day); Common::month_button("exp_month", $start_month); Common::year_button("exp_year", $start_year); ?></td>
 	</tr>
 	<tr>
 		<td align="right"><?php echo JText::_('BILLABLE'); ?>:</td>
-		<td><input type="text" name="url" size="42" /></td>
+		<td>
+			<select name="billable">
+			<option value="0"><?php echo JText::_('BILLABLE'); ?></option>
+			<option value="1"><?php echo JText::_('INTERNAL'); ?></option>
+			<option value="2"><?php echo JText::_('PERSONAL'); ?></option>
+			</select>
+			</td>
 	</tr>
 	<tr>
 		<td align="right" valign="top"><?php echo JText::_('AMOUNT'); ?>:</td>
-		<td><?php Common::multi_user_select_list("assigned[]"); ?></td>
+		<td><input type="text" name="amount" size="15" /></td>
 	</tr>
 	<tr>
-			<!-- table width="100%" border="0" class="table_bottom_panel" -->
-		<td align="center">
+		<td></td>
+		<td>
 			<input type="submit" name="add" value="<?php echo JText::_('ADD_NEW_EXPENSE'); ?>" />
 		</td>
 	</tr>
