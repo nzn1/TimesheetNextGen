2efa6cd8cffaa19bdac085574b63e55ac5f48075
wrightsonm <wrightsonm@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2011-10-09 16:22:46 +0000

    added a developer prerequisite class
    The purpose of this class is to highlight changes in the system that need user intervention.

---
+++ branches/txsheet-2.0-demo/include/developer.class.php
@@ -0,0 +1,35 @@
+<?php
+class Developer{
+  
+  public static function checkForDeveloperPrerequisites(){
+  
+    self::timesTable();
+    
+    
+    
+  
+  }
+  
+  
+  public static function timesTable(){
+  //ALTER TABLE __TABLE_PREFIX__times CHANGE `uid` `username` varchar(32) DEFAULT '' NOT NULL
+    $q = "SHOW COLUMNS FROM ".tbl::getTimesTable().";";    
+    $data = Database::getInstance()->sql($q,Database::TYPE_OBJECT);
+    
+    foreach($data as $obj){
+      if($obj->Field == 'uid'){
+      
+        $msg = "The column uid has been modified to username.<br />"
+            ."Look in the upgrade sql.in file for the line below and update your db:<br />"
+            ."ALTER TABLE __TABLE_PREFIX__times CHANGE `uid` `username` varchar(32) DEFAULT '' NOT NULL";
+        $title = "Database Update Required";        
+        Errorhandler::fatalError($msg.ppr($obj,'SQL Problem Column',true),$title,$title);
+      }
+    }
+  
+  }
+
+
+}
+
+?>
