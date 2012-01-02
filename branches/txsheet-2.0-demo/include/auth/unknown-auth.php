5f32d0d88ac1be095ba25294fccfe9638fd041cd
wrightsonm <wrightsonm@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2012-01-02 18:28:19 +0000

    corrected some templating issues

--- branches/txsheet-2.0-demo/include/auth/unknown-auth.php
+++ branches/txsheet-2.0-demo/include/auth/unknown-auth.php
@@ -6,7 +6,7 @@ echo "<h3>Error: Authorisation Privileges haven't been Configured</h3>";
 echo "<p>This page does not have any privileges configured.  Consequently, it cannot be viewed.</p>";
 echo "<p>Please contact the webmaster with the following reference: <strong>".$_SERVER['REQUEST_URI']."</strong></p><br />";
 
-echo"<p>You are currently logged on as <strong>".Site::getSession()->getUserInfo()->getUsername()."</strong></p>";
+echo"<p>You are currently logged on as <strong>".$_SESSION['loggedInUser']."</strong></p>";
 echo"<p>You require the privilege <strong>".PageElements::getPageAuth()->authGroup." - ".PageElements::getPageAuth()->authName."</strong></p>";
 echo "<p>Please use the main menu to navigate to another page.</p>
   <hr />
