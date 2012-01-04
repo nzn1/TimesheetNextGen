140f739367e8ba3561fca0799042e9ceb5147996
peterlazarus <peterlazarus@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2012-01-04 10:15:26 +0000

    Restored deleted folder

---
+++ themes/txsheet2/config.php
@@ -0,0 +1,11 @@
+<?php
+	
+//	self::addFile('content',Rewrite::getContent());
+	
+	//self::addFile('menu','themes/'.PageElements::getTheme().'/menu.php');
+	self::addFile('tsx_banner','themes/'.PageElements::getTheme().'/banner.inc');
+	self::addFile('tsx_footer','themes/'.PageElements::getTheme().'/footer.inc');
+	self::setError404('404.php');
+  
+	self::addElement(new FunctionTag('relativeRoot','relRoot',FunctionTag::TYPE_FUNC));
+?>
