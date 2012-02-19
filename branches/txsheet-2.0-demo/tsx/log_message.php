8eeea9f3ac7609ebde003502cec5d3db989b96bb
peterlazarus <peterlazarus@1a63d2e7-1745-0410-8cfb-8b68dfa47464>
2012-02-19 00:18:18 +0000

    Change log_message to use new css. Fix issues with other modules

--- branches/txsheet-2.0-demo/tsx/log_message.php
+++ branches/txsheet-2.0-demo/tsx/log_message.php
@@ -12,9 +12,10 @@ $ca = new ClockAction();
 // create the command menu cancel option
 Site::getCommandMenu()->add(new TextCommand("Cancel", true, $ca->getDestination()."?client_id=".gbl::getClientId()."&amp;proj_id=".gbl::getProjId()."&amp;task_id=".gbl::getTaskId()."&amp;year=".gbl::getYear()."&amp;month=".gbl::getMonth()."&amp;day=".gbl::getDay()));
 
-PageElements::setHead("<title>".Config::getMainTitle()." | Clock off - Enter log message</title>");
+PageElements::setHead("<title>".Config::getMainTitle()." | " .JText::_('LOG_MESSAGE')."</title>");
+PageElements::setTheme('txsheet2');
 ?>
-
+<h1><?php echo JText::_('LOG_MESSAGE'); ?></h1>
 <form action="<?php echo Config::getRelativeRoot(); ?>/clock_action" method="post">
 	<input type="hidden" name="origin" value="<?php echo $ca->getOrigin(); ?>" />
 	<input type="hidden" name="destination" value="<?php echo $ca->getDestination(); ?>" />
@@ -31,47 +32,10 @@ PageElements::setHead("<title>".Config::getMainTitle()." | Clock off - Enter log
 	<input type="hidden" name="clockonoff" value="<?php echo $ca->getClockOnOff(); ?>" />
 	<input type="hidden" name="log_message_presented" value="1" />
 
-<table width="600" align="center" border="0" cellspacing="0" cellpadding="0">
-	<tr>
-		<td width="100%" class="face_padding_cell">
-
-				<table width="100%" border="0">
-					<tr>
-						<td align="left" class="outer_table_heading">
-							Enter Log Message
-						</td>
-					</tr>
-				</table>
-
-	<table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" class="outer_table">
-		<tr>
-			<td>
-				<table width="100%" border="0" cellpadding="1" cellspacing="2" class="table_body">
-					<tr class="inner_table_head">
-						<td class="inner_table_column_heading">Please Enter Log message: (max 255 characters)</td>
-					</tr>
-					<tr>
-						<td>
-							<textarea name="log_message" cols="60" rows="4" style="width: 100%;"></textarea>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<table width="100%" border="0" class="table_bottom_panel">
-					<tr>
-						<td align="center">
-							<input type="submit" value="Done" />
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</table>
-		</td>
-	</tr>
-</table>
+<div id="inputArea">
+	<div><label><?php echo JText::_('ENTER_LOG_MESSAGE'); ?></label>
+			<textarea name="log_message" cols="60" rows="4"></textarea></div>
+<div><label></label><input type="submit" name="add" value="<?php echo JText::_('SUBMIT'); ?>" /></div>
+</div>
 
 </form>
